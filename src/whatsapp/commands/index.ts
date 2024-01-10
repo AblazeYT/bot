e764defc7fb5442deb9215c1e9b5f137f0c78aa3
Linkwarrior75 <94568482+Linkwarrior75@users.noreply.github.com>
2024-01-10 19:34:14 +0000

    pingpong oops
    forgot to add to index (big oopsie)

--- src/whatsapp/commands/index.ts
+++ src/whatsapp/commands/index.ts
@@ -1,7 +1,11 @@
 import HelpCommand from "./Help";
 import EveryoneCommand from "./Everyone";
+import PingCommand from "./Ping";
+import PongCommand from "./Pong";
 
 export default [
     HelpCommand,
     EveryoneCommand,
+    PingCommand,
+    PongCommand,
 ];
