77e92b08dabc5addc436925ebee1d6264f1e2891
Linkwarrior75 <94568482+Linkwarrior75@users.noreply.github.com>
2024-01-10 20:00:48 +0000

    nuh uh index
    forgor again

--- src/whatsapp/commands/index.ts
+++ src/whatsapp/commands/index.ts
@@ -2,10 +2,12 @@ import HelpCommand from "./Help";
 import EveryoneCommand from "./Everyone";
 import PingCommand from "./Ping";
 import PongCommand from "./Pong";
+import NuhuhCommand from "./Nuhuh";
 
 export default [
     HelpCommand,
     EveryoneCommand,
     PingCommand,
     PongCommand,
+    NuhuhCommand,
 ];
