dad198de455d3a0c8ca8c5bc3ee320bb9536e83d
apricotwtf <hey@apricot.wtf>
2024-01-14 21:12:08 +0000

    attempt to fix prodia

--- src/common/ai/image.ts
+++ src/common/ai/image.ts
@@ -3,33 +3,30 @@ export async function generate_image(prompt: string): Promise<Buffer> {
         new: "true",
         prompt: prompt,
         model: "Realistic_Vision_V2.0.safetensors [79587710]",
-        negative_prompt: "((((ugly)))), (((duplicate))), ((morbid)), ((mutilated)), out of frame, extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), ((ugly)), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck))), ((((ugly)))), (((duplicate))), ((morbid)), ((mutilated)), out of frame, extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), ((ugly)), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck))) , ((((ugly)))), (((duplicate))), ((morbid)), ((mutilated)), out of frame, extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), ((ugly)), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck)))",
+        negative_prompt: "",
 
-        steps: "30",
-        cfg: "9.5",
+        steps: "20",
         seed: Math.floor(10_000 + (Math.random() * 89_999)).toString(),
         sampler: "Euler",
         aspect_ratio: "square",
     });
 
     const res = await fetch("https://api.prodia.com/generate?" + urlParams.toString(), {
-        method: "GET",
-        headers: {
-            "authority": "api.prodia.com",
+        "headers": {
             "accept": "*/*",
-            "accept-language": "en-US,en;q=0.6",
-            "dnt": "1",
-            "origin": "https://app.prodia.com",
-            "referer": "https://app.prodia.com/",
-            "sec-ch-ua": '"Brave";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,ru;q=0.7",
+            "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
             "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Linux"',
+            "sec-ch-ua-platform": "\"Windows\"",
             "sec-fetch-dest": "empty",
             "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-site",
-            "sec-gpc": "1",
-            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
-        },
+            "sec-fetch-site": "same-site"
+          },
+          "referrer": "https://app.prodia.com/",
+          "referrerPolicy": "strict-origin-when-cross-origin",
+          "method": "GET",
+          "mode": "cors",
+          "credentials": "omit"
     });
     const jobData = <{ job: string | undefined }> await res.json();
 
