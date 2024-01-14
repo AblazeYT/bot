840fb1d1b1e84a9f6ede6a7c2414ac2596b25969
apricotwtf <hey@apricot.wtf>
2024-01-14 21:50:07 +0000

    i hate this

--- src/common/ai/image.ts
+++ src/common/ai/image.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import proxyAgent from "https-proxy-agent";
+import * as proxyAgent from "https-proxy-agent";
 import getFetch from "./sob.js";
 const proxies = `45.90.248.189:12323:14aae7b844eaa:664b9de4cd
 194.124.144.109:12323:14aae7b844eaa:664b9de4cd
