48f04d5ba38ebea9bbb9d797e9e2ce86935d1b6e
apricotwtf <hey@apricot.wtf>
2024-01-14 21:39:32 +0000

    give it a little bit of spice

--- src/common/ai/image.ts
+++ src/common/ai/image.ts
@@ -1,5 +1,12 @@
+import proxyAgent from "https-proxy-agent";
+const proxies = `45.90.248.189:12323:14aae7b844eaa:664b9de4cd
+194.124.144.109:12323:14aae7b844eaa:664b9de4cd
+45.91.33.180:12323:14aae7b844eaa:664b9de4cd
+185.232.167.157:12323:14aae7b844eaa:664b9de4cd
+193.3.176.193:12323:14aae7b844eaa:664b9de4cd`
 export async function generate_image(prompt: string): Promise<Buffer> {
-
+    const fetch = await import("node-fetch").then(m => m.default);
+    const proxy = proxies.split("\n")[Math.floor(Math.random() * proxies.split("\n").length)].split(":");
     const res = await fetch("https://api.prodia.com/generate?new=true&prompt=" + encodeURIComponent(prompt) + "&model=Realistic_Vision_V5.0.safetensors+%5B614d1063%5D&negative_prompt=&steps=20&cfg=7&seed=3296971322&sampler=DPM%2B%2B+2M+Karras&aspect_ratio=square", {
         "headers": {
             "accept": "*/*",
@@ -15,7 +22,8 @@ export async function generate_image(prompt: string): Promise<Buffer> {
           "referrerPolicy": "strict-origin-when-cross-origin",
           "method": "GET",
           "mode": "cors",
-          "credentials": "omit"
+          "credentials": "omit",
+          agent: new proxyAgent.HttpsProxyAgent(`http://${proxy[2]}:${proxy[3]}@${proxy[0]}:${proxy[1]}`)
     });
     const jobData = <{ job: string | undefined }> await res.json();
 
@@ -31,6 +39,7 @@ export async function generate_image(prompt: string): Promise<Buffer> {
                 authority: "api.prodia.com",
                 accept: "*/*",
             },
+            agent: new proxyAgent.HttpsProxyAgent(`http://${proxy[2]}:${proxy[3]}@${proxy[0]}:${proxy[1]}`)
         }).then(res => res.json());
 
         if (image_data.status === "succeeded") {
